4a903c87fdae16a5c12f0cd92d455df71e96e95b
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-04-03 16:19:49 +0900

    초기 커밋.

---
+++ ConsoleApplication30/ConsoleApplication30.cpp
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main(void)
+{
+    int KRW = 0, rate = 0;
+
+    printf("KRW?");
+    scanf_s("%d", &KRW);
+
+    printf("원달러환률");
+    scanf_s("%d", &rate);
+
+
+    printf("KRW %d=USD%0.2lf", KRW, double(KRW) / double(rate));
+}
